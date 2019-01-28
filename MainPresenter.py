bca19e19303847d10ed3288fe8fbdb40d543676e
woolf <29702137+S0Ulle33@users.noreply.github.com>
2019-01-28 20:30:53 +0300

    Update scan technique from raw sockets to SYN scan with scapy

--- MainPresenter.py
+++ MainPresenter.py
@@ -7,8 +7,8 @@ from PyQt5.Qt import QThread, pyqtSignal
 from PyQt5.QtCore import QObject, pyqtSlot
 
 import CoreModel
-import Parser
-from RawTCP import TCPHeader, handle_packet, tcp_checksum
+import parser
+from scaner import SYNScanner
 
 
 class MainPresenter:
@@ -17,172 +17,136 @@ class MainPresenter:
         self.ui = ui
         self.coreModel = CoreModel
 
-        self.is_scan_enabled = False
-        self.scan_queue = queue.Queue()
-        self.__listener_thread = None
-        self.__scan_threads = []
+        self.isScanEnabled = False
+        self.scanQueue = queue.Queue()
+        self.__listenerThread = None
+        self.__scanThreads = []
 
-    def start_scan(self, ip_ranges, ports, threads_number, timeout):
-        ip_ranges = Parser.getCIDRFromRanges(ip_ranges)
-        ports = Parser.getPortsFromString(ports)
-        threads_number = int(threads_number)
+    def startScan(self, IPRanges, ports, threadsNumber, timeout):
+        IPRanges = parser.getCIDRFromRanges(IPRanges)
+        ports = parser.getPortsFromString(ports)
+        threadsNumber = int(threadsNumber)
         if not timeout:
             timeout = 3
         else:
             timeout = int(timeout)
-
-        target = self.coreModel.Target(ip_ranges, ports)
-
-        listener_worker = ListenerWorker(target)
-        listener_thread = QThread()
-        listener_worker.log_signal.connect(self.log_text)
-        listener_worker.moveToThread(listener_thread)
-        listener_worker.finished_signal.connect(listener_thread.exit)
-        listener_worker.finished_signal.connect(self.worker_exit)
-        listener_thread.started.connect(listener_worker.work)
-        self.__listener_thread = (listener_worker, listener_thread)
-        listener_thread.start()
+        scaner = SYNScanner(ports, timeout)
+        target = self.coreModel.Target(IPRanges, ports)
 
         for ip in target.range():
-            self.scan_queue.put(str(ip))
-        for _ in range(threads_number):
-            scan_worker = ScanWorker(self.scan_queue, ports, self)
-            scan_thread = QThread()
-            scan_worker.log.connect(self.log_text)
-            scan_worker.moveToThread(scan_thread)
-            scan_worker.finished.connect(scan_thread.exit)
-            scan_worker.finished.connect(self.worker_exit)
-            scan_thread.started.connect(scan_worker.work)
-            self.__scan_threads.append((scan_worker, scan_thread))
-        self.set_running_threads_label(len(self.__scan_threads) + 1)
-        [t[1].start() for t in self.__scan_threads]
+            self.scanQueue.put(str(ip))
+        for _ in range(threadsNumber):
+            scanWorker = ScanWorker(scaner, self.scanQueue)
+            scanThread = QThread()
+            scanWorker.logSignal.connect(self.logText)
+            scanWorker.moveToThread(scanThread)
+            scanWorker.finishedSignal.connect(scanThread.exit)
+            scanWorker.finishedSignal.connect(self.workerExit)
+            scanThread.started.connect(scanWorker.work)
+            self.__scanThreads.append((scanWorker, scanThread))
 
+        self.setRunningThreadsLabel(len(self.__scanThreads))
+        [t[1].start() for t in self.__scanThreads]
 
-    def worker_exit(self):
-        self.set_running_threads_label(int(self.ui.currentThreadsLabel.text()) - 1)
-        if all(not worker.is_running for worker, _ in self.__scan_threads) and not self.__listener_thread[0].is_running:
-            self.is_scan_enabled = False
-            self.__scan_threads.clear()
-            with self.scan_queue.mutex:
-                self.scan_queue.queue.clear()
-                self.scan_queue.all_tasks_done.notify_all()
-                self.scan_queue.unfinished_tasks = 0
+    def workerExit(self):
+        self.setRunningThreadsLabel(int(self.ui.currentThreadsLabel.text()) - 1)
+        if all(not worker.isRunning for worker, _ in self.__scanThreads):
+            self.isScanEnabled = False
+            self.__scanThreads.clear()
+            with self.scanQueue.mutex:
+                self.scanQueue.queue.clear()
+                self.scanQueue.all_tasks_done.notify_all()
+                self.scanQueue.unfinishedSignal_tasks = 0
             self.ui.startButton.setText("Start")
 
-    def stop_scan(self):
-        self.stop_workers()
-        self.is_scan_enabled = False
-        self.__scan_threads.clear()
-        with self.scan_queue.mutex:
-            self.scan_queue.queue.clear()
-            self.scan_queue.all_tasks_done.notify_all()
-            self.scan_queue.unfinished_tasks = 0
-        self.ui.startButton.setText("Start")
-        self.set_running_threads_label("0")
-
-    def stop_workers(self):
+    def stopScan(self):
         try:
-            for worker, _  in self.__scan_threads:
-                if worker.is_running:
+            for worker, _ in self.__scanThreads:
+                if worker.isRunning:
                     worker.stop()
-            if self.__listener_thread[0].is_running:
-                self.__listener_thread[0].stop()
         except Exception as e:
-            self.log_text(e)
+            self.logText(e)
 
-    def log_text(self, text):
+    def logText(self, text):
         print(text)
         self.ui.dataText.append(
             "[" + str(datetime.datetime.now()) + "] " + str(text))
 
-    def set_running_threads_label(self, threadNumber):
+    def setRunningThreadsLabel(self, threadNumber):
         self.ui.currentThreadsLabel.setText(str(threadNumber))
 
-
+"""
 class ListenerWorker(QObject):
 
-    finished_signal = pyqtSignal()
-    log_signal = pyqtSignal(str)
+    finishedSignal = pyqtSignal()
+    logSignal = pyqtSignal(str)
 
-    def __init__(self, target, **kwds):
+    def __init__(self, target, scanThreads,**kwds):
         super().__init__(**kwds)
         self.target = target
-        self.ip_ports = {str(ip):self.target.ports[:] for ip in list(self.target.range())}
-        self.listen_socket = socket.socket(
-            socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        self.is_running = True
+        self.IPPortDict = {str(ip):self.target.ports[:] for ip in list(self.target.range())}
+        self.listenSocket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+        self.isRunning = True
+
+        self.__scanThreads = scanThreads
 
     @pyqtSlot()
     def work(self):
-        self.log_signal.emit("ListenerWorker::work()")
+        self.logSignal.emit("ListenerWorker::work()")
         MAX_RETRIES = 5
         RETRIES = 0
-        while self.is_running:
-            if not all([item[1] for item in self.ip_ports.items()]) or RETRIES >= MAX_RETRIES:
+        while self.isRunning:
+            if not (all([item[1] for item in self.IPPortDict.items()])
+                or RETRIES >= MAX_RETRIES) and all(not t.isRunning for _, t in self.__scanThreads):
                 self.stop()
-            raw_packet = self.listen_socket.recvfrom(65565)
-            is_open, ret = handle_packet(raw_packet)
-            src_addr, src_port = ret
-            if self.target.is_target(src_addr, src_port):
-                if is_open:
-                    self.log_signal.emit(f"{src_addr} has open port: {src_port}")
-                self.ip_ports[src_addr].remove(src_port)
+                break
+            rawPacket = self.listenSocket.recvfrom(65565)
+            isOpen, ret = handle_packet(rawPacket)
+            srcAddress, srcPort = ret
+            if self.target.isTarget(srcAddress, srcPort):
+                if isOpen:
+                    self.logSignal.emit(f"{srcAddress} has open port: {srcPort}")
+                try:
+                    self.IPPortDict[srcAddress].remove(srcPort)
+                except ValueError as e:
+                    self.logSignal.emit(f"Error in self.IPPortDict[srcAddress].remove(srcPort): {e} (no {srcPort} in IPPortDict[{srcAddress}])")
+                    pass
                 RETRIES = 0
             else:
                 RETRIES += 1
 
     def stop(self):
-        self.log_signal.emit("ListenerWorker::stop()")
-        self.is_running = False
-        self.finished_signal.emit()
-
+        self.logSignal.emit("ListenerWorker::stop()")
+        self.isRunning = False
+        self.listenSocket.close()
+        self.finishedSignal.emit()
+"""
 
 class ScanWorker(QObject):
 
-    finished = pyqtSignal()
-    log = pyqtSignal(str)
+    finishedSignal = pyqtSignal()
+    logSignal = pyqtSignal(str)
 
-    def __init__(self, scan_queue, ports, presenter, **kwds):
+    def __init__(self, scaner, scanQueue, **kwds):
         super().__init__(**kwds)
 
-        self.scan_queue = scan_queue
-        self.ports = ports
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-        self.presenter = presenter
-        self.source_ips = {}
-        self.is_running = True
-
-    def get_source_ip(self, dst_addr):
-        try:
-            if dst_addr in self.source_ips:
-                return self.source_ips[dst_addr]
-            else:
-                self.source_ips[dst_addr] = [(s.connect((dst_addr, 53)), s.getsockname()[0], s.close(
-                )) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
-                return self.source_ips[dst_addr]
-        except Exception:
-            print("Something went wrong in get_source_ip, results might be wrong\n")
+        self.scaner = scaner
+        self.scanQueue = scanQueue
+        self.isRunning = True
 
     @pyqtSlot()
     def work(self):
-        self.log.emit("ScanWorker::work()")
-        while self.is_running:
-            if self.scan_queue.empty():
+        #self.logSignal.emit("ScanWorker::work()")
+        while self.isRunning:
+            if self.scanQueue.empty():
                 self.stop()
-            dest_ip = self.scan_queue.get()
-            for dst_port in self.ports:
-                src_addr = self.get_source_ip(dest_ip)
-                src_port = 54321
-                make_tcpheader = TCPHeader(src_port, dst_port)
-                tcp_header = make_tcpheader.get_struct()
-                packet = make_tcpheader.get_struct(check=tcp_checksum(src_addr, dest_ip, tcp_header), checksummed=True)
-                try:
-                    self.socket.sendto(packet, (dest_ip, 0))
-                except Exception as e:
-                    print(f"Error utilizing raw socket in send_raw_syn: {e}\n")
-            self.scan_queue.task_done()
+            dstIP = self.scanQueue.get()
+            openPorts = self.scaner.scan(dstIP)
+            for openPort in openPorts:
+                self.logSignal.emit(f"{dstIP} has open port: {openPort}")
+            self.scanQueue.task_done()
 
     def stop(self):
-        self.log.emit("ScanWorker::stop()")
-        self.is_running = False
-        self.finished.emit()
+        #self.logSignal.emit("ScanWorker::stop()")
+        self.isRunning = False
+        self.finishedSignal.emit()
